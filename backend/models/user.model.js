f1c17627a98f331075398e22f1e7f8d22885209e
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-03-30 04:20:59 -0400

    Send Message API

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     fullname: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 8
+      type: String,
+      required: true,
+      minlength: 8,
     },
     gender: {
-        type: String,
-        required: true,
-        enum: ["male", "female"]
+      type: String,
+      required: true,
+      enum: ["male", "female"],
     },
     profilePic: {
-        type: String,
-        default: ""
-    }
-});
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // Declarations
@@ -11,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
