ac7c50a2505ebf7c88f90c7b17a604b2e788db75
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-04-04 03:22:09 -0400

    Homepage design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Signup from "./pages/signup/Signup";
+import Home from "./pages/home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <Signup />
+        <Home />
       </div>
     </>
   );
