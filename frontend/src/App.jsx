e95632bcd614fe270ee690a29d9a9d78c39d267d
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-04-05 02:52:13 -0400

    Add Signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Signup from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
   const [count, setCount] = useState(0);
+  const { authUser } = useAuthContext();
 
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={authUser ? <Home /> : <Navigate to="/" />} />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <Signup />}
+          />
         </Routes>
+        <Toaster />
       </div>
     </>
   );
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import GenderCheckBox from "./GenderCheckBox";
 import { Link } from "react-router-dom";
+import useSignup from "../../hooks/useSignup";
 
 const Signup = () => {
+  const [inputs, setInputs] = useState({
+    fullname: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -10,7 +29,7 @@ const Signup = () => {
           Sign Up <span className="text-blue-500"> YoChat</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
@@ -19,6 +38,10 @@ const Signup = () => {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered h-10"
+              value={inputs.fullname}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullname: e.target.value })
+              }
             />
           </div>
           <div>
@@ -29,6 +52,10 @@ const Signup = () => {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
           <div>
@@ -39,20 +66,31 @@ const Signup = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
           <div>
             <label className="label p-2">
-              <span className="text-base label-text"> ConfirmPassword</span>
+              <span className="text-base label-text"> Confirm Password</span>
             </label>
             <input
               type="password"
               placeholder="Enter Password Again"
               className="w-full input input-bordered h-10"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           </div>
 
-          <GenderCheckBox />
+          <GenderCheckBox
+            onCheckBoxChange={handleCheckboxChange}
+            selectedGender={inputs.gender}
+          />
 
           <Link
             to="/login"
